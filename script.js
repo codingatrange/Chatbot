9ed70457f4b73c85edb58a29c1a268fbf7fded0b
codingatrange <165120622+codingatrange@users.noreply.github.com>
2025-11-23 23:05:22 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function appendValue(value) {
+    document.getElementById('display').value += value;
+}
+
+function calculate() {
+    try {
+        const display = document.getElementById('display');
+        display.value = eval(display.value);
+    } catch (error) {
+        alert('Invalid expression');
+    }
+}
